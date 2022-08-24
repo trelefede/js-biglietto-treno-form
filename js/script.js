721916da1ecc52c8bc23eb5e57a6a9b8ad39cc55
TreleaniFederico <treleani.federico@gmail.com>
2022-08-24 16:27:43 +0200

    add inner html to print result

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
+console.log('js ok')
+
 const priceKm = 0.21;
 
+const totalPrice = document.getElementById("totalPrice");
+
 // users data input
-const distance = parseInt(prompt('Quanti km vuoi percorrere?'));
-if (isNaN(distance)) {
-    alert('Valore inserito non valido! Riprova');
-} else {
-    const age = parseInt(prompt('Quanti anni hai?'));
-    if (isNaN(age)) {
+document.getElementById("btn").addEventListener("click", function () {
+    const distance = parseInt(document.getElementById("distance").value);
+    console.log(distance);
+
+    const age = parseInt(document.getElementById("age").value);
+    console.log(age);
+
+    if (isNaN(distance) && isNaN(age)) {
         alert('Valore inserito non valido! Riprova');
     } else {
         // price calc 
         let ticketPrice = priceKm * distance;
         if (age < 18) {
             let ticketPriceYoung = ticketPrice - ((ticketPrice / 100) * 20);
-            alert('Il prezzo del viaggio con sconto Yunior è ' + ticketPriceYoung.toFixed(2) + "€")
+            totalPrice.innerHTML = ticketPriceYoung.toFixed(2) + ' €';
         } else if (age > 65) {
             let ticketPriceSenior = ticketPrice - ((ticketPrice / 100) * 40);
-            alert('Il prezzo del viaggio con sconto Senior è ' + ticketPriceSenior.toFixed(2) + "€")
+            totalPrice.innerHTML = ticketPriceSenior.toFixed(2) + ' €';
         } else {
-            alert('Il prezzo del viaggio è ' + ticketPrice.toFixed(2) + "€")
+            totalPrice.innerHTML = ticketPrice.toFixed(2) + ' €';
         }
     }
-}
+})
+
